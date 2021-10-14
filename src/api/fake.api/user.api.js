7d7c7bf946a7ffc0f8490cef385c0e459958506b
Olga Runova <ockaaa@gmail.com>
2021-10-14 11:05:11 +0300

    api replacement

--- src/api/fake.api/user.api.js
+++ src/api/fake.api/user.api.js
@@ -1,13 +1,35 @@
 import { professionsObject as professions } from './professions.api';
-
 const qualities = {
-  tedious: { _id: '67rdca3eeb7f6fgeed471198', name: 'Нудила', color: 'primary' },
-  strange: { _id: '67rdca3eeb7f6fgeed471100', name: 'Странный', color: 'secondary' },
-  buller: { _id: '67rdca3eeb7f6fgeed4711012', name: 'Троль', color: 'success' },
-  alcoholic: { _id: '67rdca3eeb7f6fgeed471101', name: 'Алкоголик', color: 'danger' },
-  handsome: { _id: '67rdca3eeb7f6fgeed471102', name: 'Красавчик', color: 'info' },
-  uncertain: { _id: '67rdca3eeb7f6fgeed471102', name: 'Неуверенный', color: 'dark' }
-
+  tedious: {
+    _id: '67rdca3eeb7f6fgeed471198',
+    name: 'Нудила',
+    color: 'primary'
+  },
+  strange: {
+    _id: '67rdca3eeb7f6fgeed471100',
+    name: 'Странный',
+    color: 'secondary'
+  },
+  buller: {
+    _id: '67rdca3eeb7f6fgeed4711012',
+    name: 'Троль',
+    color: 'success'
+  },
+  alcoholic: {
+    _id: '67rdca3eeb7f6fgeed471101',
+    name: 'Алкоголик',
+    color: 'danger'
+  },
+  handsome: {
+    _id: '67rdca3eeb7f6fgeed471103',
+    name: 'Красавчик',
+    color: 'info'
+  },
+  uncertain: {
+    _id: '67rdca3eeb7f6fgeed471102',
+    name: 'Неуверенный',
+    color: 'dark'
+  }
 };
 
 const users = [
@@ -18,7 +40,7 @@ const users = [
     qualities: [qualities.tedious, qualities.uncertain, qualities.strange],
     completedMeetings: 36,
     rate: 2.5,
-    status: false
+    bookmark: false
   },
   {
     _id: '67rdca3eeb7f6fgeed471816',
@@ -27,7 +49,7 @@ const users = [
     qualities: [qualities.buller, qualities.handsome, qualities.alcoholic],
     completedMeetings: 15,
     rate: 2.5,
-    status: false
+    bookmark: false
   },
   {
     _id: '67rdca3eeb7f6fgeed471817',
@@ -36,7 +58,7 @@ const users = [
     qualities: [qualities.buller],
     completedMeetings: 247,
     rate: 3.5,
-    status: false
+    bookmark: false
   },
   {
     _id: '67rdca3eeb7f6fgeed471818',
@@ -45,7 +67,7 @@ const users = [
     qualities: [qualities.uncertain],
     completedMeetings: 148,
     rate: 3.5,
-    status: false
+    bookmark: false
   },
   {
     _id: '67rdca3eeb7f6fgeed471819',
@@ -54,7 +76,7 @@ const users = [
     qualities: [qualities.strange, qualities.tedious],
     completedMeetings: 37,
     rate: 4.6,
-    status: false
+    bookmark: false
   },
   {
     _id: '67rdca3eeb7f6fgeed471820',
@@ -63,7 +85,7 @@ const users = [
     qualities: [qualities.strange, qualities.uncertain],
     completedMeetings: 147,
     rate: 3.5,
-    status: false
+    bookmark: false
   },
   {
     _id: '67rdca3eeb7f6fgeed471821',
@@ -72,7 +94,7 @@ const users = [
     qualities: [qualities.strange, qualities.tedious],
     completedMeetings: 72,
     rate: 3.5,
-    status: false
+    bookmark: false
   },
   {
     _id: '67rdca3eeb7f6fgeed471822',
@@ -81,7 +103,7 @@ const users = [
     qualities: [qualities.handsome],
     completedMeetings: 72,
     rate: 5,
-    status: false
+    bookmark: false
   },
   {
     _id: '67rdca3eeb7f6fgeed471823',
@@ -90,7 +112,7 @@ const users = [
     qualities: [qualities.strange, qualities.uncertain],
     completedMeetings: 17,
     rate: 4.5,
-    status: false
+    bookmark: false
   },
   {
     _id: '67rdca3eeb7f6fgeed471824',
@@ -99,7 +121,7 @@ const users = [
     qualities: [qualities.handsome, qualities.buller],
     completedMeetings: 17,
     rate: 4.5,
-    status: false
+    bookmark: false
   },
   {
     _id: '67rdca3eeb7f6fgeed47181f',
@@ -108,7 +130,7 @@ const users = [
     qualities: [qualities.uncertain, qualities.strange],
     completedMeetings: 434,
     rate: 3.5,
-    status: false
+    bookmark: false
   },
   {
     _id: '67rdca3eeb7f6fgeed47181r',
@@ -117,25 +139,40 @@ const users = [
     qualities: [qualities.handsome],
     completedMeetings: 434,
     rate: 5,
-    status: false
+    bookmark: false
   }
 ];
+if (!localStorage.getItem('users')) {
+  localStorage.setItem('users', JSON.stringify(users));
+}
 
 const fetchAll = () =>
   new Promise((resolve) => {
     window.setTimeout(function () {
-      resolve(users);
+      resolve(JSON.parse(localStorage.getItem('users')));
     }, 2000);
+  });
+const update = (id, data) =>
+  new Promise((resolve) => {
+    const users = JSON.parse(localStorage.getItem('users'));
+    const userIndex = users.findIndex((u) => u._id === id);
+    users[userIndex] = { ...users[userIndex], ...data };
+    localStorage.setItem('users', JSON.stringify(users));
+    resolve(users[userIndex]);
   });
 
 const getById = (id) =>
   new Promise((resolve) => {
     window.setTimeout(function () {
-      resolve(users.find(user => user._id === id));
+      resolve(
+        JSON.parse(localStorage.getItem('users')).find(
+          (user) => user._id === id
+        )
+      );
     }, 1000);
   });
-
 export default {
   fetchAll,
-  getById
+  getById,
+  update
 };
