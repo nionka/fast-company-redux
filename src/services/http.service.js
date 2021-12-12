61ab4054c710abf528519aa8f0ada55429d8daa8
Olga Runova <ockaaa@gmail.com>
2021-12-12 23:52:53 +0300

    added firebase

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,19 +1,44 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import config from '../config.json';
+import configFile from '../config.json';
 
-axios.defaults.baseURL = config.apiEndPoint;
+axios.defaults.baseURL = configFile.apiEndPoint;
 
-axios.interceptors.response.use(res => res, function (err) {
-  const expectedErrors = err.response && err.response.status >= 400 && err.response.status < 500;
+axios.interceptors.request.use(
+  function (config) {
+    if (configFile.isFireBase) {
+      const containSlash = /\/$/gi.test(config.url);
+      config.url = (containSlash ? config.url.slice(0, -1) : config.url) + '.json';
+    }
 
-  if (!expectedErrors) {
-    console.log(err);
-    toast.error('Что-то пошло не так. Попробуйте позже!!!');
+    return config;
+  }, function (error) {
+    return Promise.reject(error);
   }
+);
 
-  return Promise.reject(err);
-});
+function transformData (data) {
+  return data
+    ? Object.keys(data).map(key => ({ ...data[key] }))
+    : [];
+}
+
+axios.interceptors.response.use(
+  res => {
+    if (configFile.isFireBase) {
+      res.data = { content: transformData(res.data) };
+    }
+    return res;
+  }, function (err) {
+    const expectedErrors = err.response && err.response.status >= 400 && err.response.status < 500;
+
+    if (!expectedErrors) {
+      console.log(err);
+      toast.error('Что-то пошло не так. Попробуйте позже!!!');
+    }
+
+    return Promise.reject(err);
+  });
 
 const httpService = {
   get: axios.get,
