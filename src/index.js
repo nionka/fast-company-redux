1987863dd401ca235bcab1619676dae0e6a3d7a7
Olga Runova <ockaaa@gmail.com>
2022-01-13 22:05:27 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import { createStore } from './store/createStore';
+import { Provider } from 'react-redux';
+
+const store = createStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
